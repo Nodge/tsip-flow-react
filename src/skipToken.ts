727b4591cd167a2faadc4c792f0ff7c113eb620a
Maxim Zemskov <nodge@yandex.ru>
2025-10-18 19:12:41 +0200

    fix(build): enable DTS resolution to remove @tsip/types dependency

--- src/skipToken.ts
+++ src/skipToken.ts
@@ -1,2 +1,17 @@
+/**
+ * A unique symbol used to indicate that reading the flow should be skipped.
+ *
+ * When passed as an argument to flow hooks, it signals that the flow
+ * should not be read and no result will be returned.
+ *
+ * @example
+ * ```tsx
+ * const result = useFlow(shouldSkip ? skipToken : flow);
+ * ```
+ */
 export const skipToken = Symbol("skipToken");
+
+/**
+ * Type representing the {@link skipToken} symbol.
+ */
 export type SkipToken = typeof skipToken;
